3af884f9ab75a2715d881afa683b104293f097aa
Timau32 <timau32@gmail.com>
2023-04-25 15:51:22 +0600

    changed extra ports on client

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/' });
+const host = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/' });
 
 const getImage = (params: string) => host.get(`/image?id=${params}`);
 const postImage = (params: string, img: string) => host.post(`/image?id=${params}`, { img });
